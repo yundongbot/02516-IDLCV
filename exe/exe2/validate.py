570dd74516ae5b34f316d9da2d8ab7eb8894eb9f
DongYun <yundongme@gmail.com>
2024-10-28 13:03:25 +0100

    exe 2.4

--- exe/exe2/validate.py
+++ exe/exe2/validate.py
@@ -1,43 +1,45 @@
 import numpy as np
 
-def validate(y_pred, y_true):
-    y_pred = (y_pred > 0.5).astype(np.uint8).flatten()
-    y_true = (y_true > 0.5).astype(np.uint8).flatten()
-
-    dice = Dice(y_pred, y_true)
-    iou = IoU(y_pred, y_true)
-    sensitivity = Sensitivity(y_pred, y_true)
-    specificity = Specificity(y_pred, y_true)
-    precision = Precision(y_pred, y_true)
-
-    print(f"Dice: {dice:.4f},\n IoU: {iou:.4f},\n Sensitivity: {sensitivity:.4f},\n Specificity: {specificity:.4f},\n Precision: {precision:.4f}")
-    return dice, iou, sensitivity, specificity, precision
+def validate(y_pred_batch, y_true_batch):
+    # Check if y_pred_batch has values greater than 1
+    if np.any(y_pred_batch > 1):
+        print("Warning: y_pred_batch contains values greater than 1")
+        # y_pred_batch = np.clip(y_pred_batch, 0, 1)  # Clip values to [0, 1] range
+    print(y_pred_batch.shape)
+    size = len(y_pred_batch)
+    dice, iou, sensitivity, specificity, precision = 0, 0, 0, 0, 0
+    for y_pred, y_true in zip(y_pred_batch, y_true_batch):
+        y_pred = (y_pred > 0.5).astype(bool).flatten()
+        y_true = (y_true > 0.5).astype(bool).flatten()
+        dice += Dice(y_pred, y_true)
+        iou += IoU(y_pred, y_true)
+        sensitivity += Sensitivity(y_pred, y_true)
+        specificity += Specificity(y_pred, y_true)
+        precision += Precision(y_pred, y_true)
+    return dice/size, iou/size, sensitivity/size, specificity/size, precision/size
 
 def Dice(y_pred, y_true, smooth=1e-6):
-    intersection = np.sum(y_pred * y_true)
+    intersection = np.sum(y_pred & y_true)
     sum_pred = np.sum(y_pred)
     sum_true = np.sum(y_true)
-    return (2. * intersection + smooth) / (sum_pred + sum_true + smooth)
+    return (2. * intersection) / (sum_pred + sum_true + smooth)
 
 def IoU(y_pred, y_true, smooth=1e-6):
-    intersection = np.sum(y_pred * y_true)
+    intersection = np.sum(y_pred & y_true)
     union = np.sum(y_pred) + np.sum(y_true) - intersection
-    return (intersection + smooth) / (union + smooth)
+    return (intersection) / (union + smooth)
 
 def Sensitivity(y_pred, y_true, smooth=1e-6):
-    TP = np.sum(y_pred * y_true)
-    FN = np.sum(y_true) - TP
-    return (TP + smooth) / (TP + FN + smooth)
+    TP = (y_pred & y_true).sum()
+    FN = ((~y_pred) & y_true).sum()
+    return (TP) / (TP + FN + smooth)
 
 def Specificity(y_pred, y_true, smooth=1e-6):
-    TP = np.sum(y_pred * y_true)
-    FP = np.sum(y_pred) - TP
-    TN = np.sum((1 - y_pred) * (1 - y_true))
-    return (TN + smooth) / (TN + FP + smooth)
+    TN = ((~y_pred) & (~y_true)).sum()
+    FP = (y_pred & (~y_true)).sum()
+    return (TN) / (TN + FP + smooth)
 
 def Precision(y_pred, y_true, smooth=1e-6):
-    TP = np.sum(y_pred * y_true)
-    FP = np.sum(y_pred) - TP
-    TN = np.sum((1 - y_pred) * (1 - y_true))
-    FN = np.sum(y_true) - TP
-    return (TP + smooth) / (TP + FP + smooth)
+    intersection = np.sum(y_pred & y_true)
+    sum_true = np.sum(y_true)
+    return intersection / (sum_true + smooth)
